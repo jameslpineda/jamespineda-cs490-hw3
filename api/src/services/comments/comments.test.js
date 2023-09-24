86ca6a1e6f91e78fb82da322bef8d12f26029ccf
James Pineda <jlp4@njit.edu>
2023-09-24 18:20:04 -0400

    created moderator role and implemented RBAC

--- api/src/services/comments/comments.test.js
+++ api/src/services/comments/comments.test.js
@@ -1,5 +1,8 @@
-import { comments, createComment } from './comments'
+import { AuthenticationError, ForbiddenError } from '@redwoodjs/graphql-server'
+
 import { db } from 'src/lib/db'
+
+import { comments, createComment, deleteComment } from './comments'
 
 // Generated boilerplate tests do not account for all circumstances
 // and can fail without adjustments, e.g. Float.
@@ -36,4 +39,42 @@ describe('comments', () => {
     expect(comment.postId).toEqual(scenario.post.bark.id)
     expect(comment.createdAt).not.toEqual(null)
   })
+
+  scenario('allows a moderator to delete a comment', async (scenario) => {
+    mockCurrentUser({ roles: ['moderator'] })
+
+    const comment = await deleteComment({
+      id: scenario.comment.jane.id
+    })
+    expect(comment.id).toEqual(scenario.comment.jane.id)
+
+    const result = await comments({ postId: scenario.comment.jane.postId })
+    expect(result.length).toEqual(0)
+  })
+
+  scenario(
+    'does not allow a non-moderator to delete a comment',
+    async (scenario) => {
+      mockCurrentUser({ roles: 'user' })
+
+      expect(() =>
+        deleteComment({
+          id: scenario.comment.jane.id
+        })
+      ).toThrow(ForbiddenError)
+    }
+  )
+
+  scenario(
+    'does not allow a logged out user to delete a comment',
+    async (scenario) => {
+      mockCurrentUser(null)
+
+      expect(() =>
+        deleteComment({
+          id: scenario.comment.jane.id
+        })
+      ).toThrow(AuthenticationError)
+    }
+  )
 })
